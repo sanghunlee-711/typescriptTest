1741ae2e1c37b3028534810e014afd89635788ed
sanghun lee <sanghunlee@sanghunui-MacBookAir.local>
2020-12-29 13:43:18 +0900

    Install: Sass - Check Sass in App.scss -  map method and fetch test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./App.scss";
 
-function App(): any {
+function App() {
+  const [buydata, setBuydata] = useState<[]>([]);
+
+  //getData function
+  function api(url: string) {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((res) => setBuydata(res.data));
+  }
+
+  useEffect(() => {
+    const url =
+      "https://tpay-coding-test.s3.ap-northeast-2.amazonaws.com/data.json";
+    api(url);
+  }, []);
+
+  function checkFunction(): void {
+    // console.log(buydata.map((el) => el.id));
+    console.log(buydata);
+
+    buydata.map((data) => {
+      type dataType = {
+        created: string;
+        douzoneCode: string;
+        id: number;
+        isUsed: boolean;
+        modified: string;
+        name: string;
+        quantity: number;
+        totalPrice: number;
+        unitPrice: number;
+      };
+
+      console.log(data["totalPrice" as keyof dataType]);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Hello WOrld!</span>
+      <h2>구매요청</h2>
+      {buydata.map(function (data) {
+        type dataType = {
+          created: string;
+          douzoneCode: string;
+          id: number;
+          isUsed: boolean;
+          modified: string;
+          name: string;
+          quantity: number;
+          totalPrice: number;
+          unitPrice: number;
+        };
+        return <span>{data["totalPrice" as keyof dataType]}</span>;
+      })}
+      <h2>구매 대상 확인</h2>
+      <h2>구매 완료</h2>
     </div>
   );
 }
